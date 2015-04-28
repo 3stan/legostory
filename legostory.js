3bde1b835a92fe868fc89bac1cd02d6592858ec4
Tristan Jung <3stan@users.noreply.github.com>
2015-04-27 23:54:20 -0400

    WIP

--- legostory.js
+++ legostory.js
@@ -2,6 +2,6 @@
 
 $(document).ready(function() { 
 	var game = new Game();
-	var testView = new TownView({model: game.get('towns').at(0)});
+	var testView = new TownView(game.get('towns').at(0));
 	$("#main_app").append(testView.render());
 });
